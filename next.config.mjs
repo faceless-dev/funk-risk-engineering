245cd3a46ac5ed48b682aa83af2c9f65fea589fa
Elias Martinek <elias.martinek@lean-coders.at>
2025-05-23 19:01:28 +0200

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-let userConfig = undefined
-try {
-  // try to import ESM first
-  userConfig = await import('./v0-user-next.config.mjs')
-} catch (e) {
-  try {
-    // fallback to CJS import
-    userConfig = await import("./v0-user-next.config");
-  } catch (innerError) {
-    // ignore error
-  }
-}
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -20,32 +6,13 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    domains: ["images.unsplash.com"],
   },
   experimental: {
+    reactCompiler: true,
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-}
-
-if (userConfig) {
-  // ESM imports will have a "default" property
-  const config = userConfig.default || userConfig
-
-  for (const key in config) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...config[key],
-      }
-    } else {
-      nextConfig[key] = config[key]
-    }
-  }
-}
-
-export default nextConfig
+  reactStrictMode: true,
+};
